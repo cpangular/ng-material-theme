ea152817a9e42a3eae998d878d114763d4f2fc64
Dan Erdelt <erd@conceptual.ca>
2023-02-28 20:19:22 +0000

    renamed legacy vars

--- projects/ng-material-theme-converter/src/lib/util/makeRefColor.ts
+++ projects/ng-material-theme-converter/src/lib/util/makeRefColor.ts
@@ -2,8 +2,9 @@ import { colorCssPropertyName } from "./colorCssPropertyName";
 import { primaryColors } from "../data/ColorSets";
 
 export function makeRefColor(palette: string, name: string, colors: typeof primaryColors, contrast: boolean = false) {
+  const value = contrast ? `${name}-contrast` : name;
   return {
-    name: colorCssPropertyName(palette, contrast ? "contrast" : undefined, name, true),
+    name: colorCssPropertyName(palette, "legacy", value, true),
     light: colors.light[name + (contrast ? "-contrast" : "")],
     dark: colors.dark[name + (contrast ? "-contrast" : "")],
   };
